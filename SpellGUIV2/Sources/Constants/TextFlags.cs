a1927b6142a2ff2d14c9b31e7f4cc6db4fea4697
lyn <ren-0@live.com>
2015-03-31 21:33:36 +0200

    Targets enum is taken from TrinityCore, since current values are mostly wrong and incomplete.
    TextFlags are only 2 values which indicate if SpellName, SpellToolTip, SpellDescription and SpellRank are empty or not - these will need to be implemented as they are probably important on the client side (currently they just copy over what was set on the template spell used for the new spell).

---
+++ SpellGUIV2/Sources/Constants/TextFlags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellEditor.Sources.Constants
+{
+    enum TextFlags
+    {
+        EMPTY       = 0xFF01FC,
+        NOT_EMPTY   = 0xFF01FE,
+    }
+}
