e42aee88f8ac18c3b03fe000f88e7434649295ba
Stoneharry <stoneharryjames@gmail.com>
2015-05-09 15:12:20 +0100

    Fix imports.

--- SpellGUIV2/Sources/DBC/ItemClass.cs
+++ SpellGUIV2/Sources/DBC/ItemClass.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SpellEditor.Sources.DBC
 {
--- SpellGUIV2/Sources/DBC/SpellIconDBC.cs
+++ SpellGUIV2/Sources/DBC/SpellIconDBC.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SpellEditor.Sources.DBC
 {
