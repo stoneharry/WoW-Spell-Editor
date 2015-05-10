0d5146dc490d13e3aa7629e08b1b542a63089357
Stoneharry <stoneharryjames@gmail.com>
2015-05-10 15:05:35 +0100

    Document unknown text flags found. This field is not even read by the client it is believed.

--- SpellGUIV2/Sources/Constants/TextFlags.cs
+++ SpellGUIV2/Sources/Constants/TextFlags.cs
@@ -9,6 +9,10 @@ namespace SpellEditor.Sources.Constants
     enum TextFlags : uint
     {
         EMPTY       = 0xFF01FC,
-        NOT_EMPTY   = 0xFF01FE
+        NOT_EMPTY   = 0xFF01FE,
+        // Schlumpf: found a localization flag not being one of the two you know but 0xFF01CC,
+        // making bit 2 the "is present" bit and adding bits 5 and 6 to the set of unknown ones.
+        UNKNOWN_1   = 0xFF01CC,
+        UNKNOWN_2   = 0xFF01EE
     }
 }
