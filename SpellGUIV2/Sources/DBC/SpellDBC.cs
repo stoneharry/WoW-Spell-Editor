e3a2f772ac1172b209817981a51f9fc655df602a
stoneharry <stoneharryjames@gmail.com>
2016-12-29 18:51:38 +0000

    Fix spell loading - we know what the data should be but SQLite stores it differently, so we can safetly cast it back to the required data type.

--- SpellGUIV2/Sources/DBC/SpellDBC.cs
+++ SpellGUIV2/Sources/DBC/SpellDBC.cs
@@ -407,9 +407,13 @@ namespace SpellEditor.Sources.DBC
                 switch (Type.GetTypeCode(f.FieldType))
                 {
                     case TypeCode.UInt32:
+                        {
+                            f.SetValueForValueType(ref record, UInt32.Parse(row[f.Name].ToString()));
+                            break;
+                        }
                     case TypeCode.Int32:
                         {
-                            f.SetValueForValueType(ref record, row[f.Name]);
+                            f.SetValueForValueType(ref record, Int32.Parse(row[f.Name].ToString()));
                             break;
                         }
                     case TypeCode.Single:
