97b83c3831df55a7fcea13d49c7d8db2e9fcfdab
Ayase <137056643@qq.com>
2017-01-21 22:21:54 +0800

    Move a definition.

--- SpellGUIV2/MainWindow.xaml.cs
+++ SpellGUIV2/MainWindow.xaml.cs
@@ -40,27 +40,12 @@ public class VirtualStrTableEntry
     public UInt32 NewValue;
 };
 
-enum LocaleConstant
-{
-	LOCALE_enUS = 0,
-	LOCALE_koKR = 1,
-	LOCALE_frFR = 2,
-	LOCALE_deDE = 3,
-	LOCALE_zhCN = 4,
-	LOCALE_zhTW = 5,
-	LOCALE_esES = 6,
-	LOCALE_esMX = 7,
-	LOCALE_ruRU = 8
-};
-
 
 namespace SpellEditor
 {
     partial class MainWindow
     {
         #region DBCDefinitions
-		//todo:multilingual.Temporary define zhCN
-		private LocaleConstant Locale_language;
 
         // Begin DBCs
         private AreaTable loadAreaTable = null;
---
+++ SpellGUIV2/Sources/Constants/Locale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellEditor.Sources.Constants
+{
+	public enum LocaleConstant
+	{
+		LOCALE_enUS = 0,
+		LOCALE_koKR = 1,
+		LOCALE_frFR = 2,
+		LOCALE_deDE = 3,
+		LOCALE_zhCN = 4,
+		LOCALE_zhTW = 5,
+		LOCALE_esES = 6,
+		LOCALE_esMX = 7,
+		LOCALE_ruRU = 8
+	};
+}
