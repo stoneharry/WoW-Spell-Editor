00e9907f1a95341dd2a0ebdf716a5bb349182bfb
stoneharry <stoneharryjames@gmail.com>
2020-03-10 22:23:26 +0000

    Enhance spell duration labels for #97
    Display a friendly days/hours/minutes/seconds breakdown on the label as well as the milliseconds.

--- SpellGUIV2/Sources/DBC/SpellDuration.cs
+++ SpellGUIV2/Sources/DBC/SpellDuration.cs
@@ -20,14 +20,32 @@ namespace SpellEditor.Sources.DBC
             {
                 var record = Body.RecordMaps[i];
                 var id = (uint) record["ID"];
-
-                Label label = new Label();
-                label.Content = $"{ record["BaseDuration"] }ms BaseDuration, { record["PerLevel"] } BaseDuration, { record["MaximumDuration"] }ms MaximumDuration, { id } ID";
-
+                var baseDurStr = GetFriendlyDuration(record["BaseDuration"]);
+                var maxDurStr = GetFriendlyDuration(record["MaximumDuration"]);
+                var perLevelStr = record["PerLevel"].ToString();
+                var label = new Label
+                {
+                    Content = $"ID: {id}\nBaseDur: {baseDurStr}\nMaxDur: {maxDurStr}\nPerLevel: {perLevelStr}"
+                };
                 Lookups.Add(new DBCBoxContainer(id, label, boxIndex));
                 ++boxIndex;
             }
             Reader.CleanStringsMap();
+        }
+
+        private string GetFriendlyDuration(object data)
+        {
+            var success = double.TryParse(data.ToString() + ".00", out double floatDur);
+            if (!success)
+            {
+                return data.ToString();
+            }
+            var time = TimeSpan.FromMilliseconds(floatDur);
+            var days = time.Days > 0 ? $"{time.Days}day " : string.Empty;
+            var hours = time.Hours > 0 ? $"{time.Hours}hour " : string.Empty;
+            var minutes = time.Minutes > 0 ? $"{time.Minutes}min " : string.Empty;
+            var seconds = time.Seconds > 0 ? $"{time.Seconds}sec" : string.Empty;
+            return (days + hours + minutes + seconds).TrimEnd() + $" - {data}ms";
         }
 
         public List<DBCBoxContainer> GetAllBoxes()
