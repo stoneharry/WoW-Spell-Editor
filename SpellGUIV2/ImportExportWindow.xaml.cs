fa5ab6a3ffd0a2e5058f4911a81e4cea4c46971f
stoneharry <stoneharryjames@gmail.com>
2022-01-22 20:38:55 +0000

    Improve the Import/Export window
    Opening the Import/Export window should now happen almost immediately and the checkboxes will be asynchronously updated as the database information is loaded.
    
    Refactor the ScrollViewer to only cover the Import/Export DBC grids rather than the entire window.

--- SpellGUIV2/ImportExportWindow.xaml.cs
+++ SpellGUIV2/ImportExportWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
@@ -59,20 +61,7 @@ namespace SpellEditor
             var contents = ImportGridDbcs.Children;
             if (contents.Count > 0)
                 return;
-            foreach (var binding in BindingManager.GetInstance().GetAllBindings())
-            {
-                var numRows = binding.GetNumRowsInTable(_Adapter);
-                contents.Add(new CheckBox
-                {
-                    Name = binding.Name + "ImportCheckBox",
-                    Content = $"{binding.Name}.dbc {(numRows > 0 ? $"- {numRows} rows" : "")}",
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Margin = new Thickness(1),
-                    IsEnabled = numRows == 0,
-                    IsChecked = numRows == 0 && IsDefaultImport(binding.Name.ToLower())
-                });
-            }
+            BuildDatabaseListGrid("ImportCheckBox", contents);
         }
 
         private void BuildExportTab()
@@ -80,20 +69,49 @@ namespace SpellEditor
             var contents = ExportGridDbcs.Children;
             if (contents.Count > 0)
                 return;
-            foreach (var binding in BindingManager.GetInstance().GetAllBindings())
+            BuildDatabaseListGrid("ExportCheckBox", contents);
+        }
+
+        private void BuildDatabaseListGrid(string name, UIElementCollection contents)
+        {
+            var bindings = new List<Binding>(BindingManager.GetInstance().GetAllBindings());
+            // Build initial checkboxes
+            bindings.ForEach((binding) =>
+                contents.Add(
+                    new CheckBox
+                    {
+                        Name = binding.Name + name,
+                        Content = $"{binding.Name} Loading...",
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Margin = new Thickness(1),
+                        IsEnabled = false,
+                        IsChecked = false
+                    }
+                ));
+            // Populate all contents asynchronously (expensive)
+            Task.Run(() => bindings.AsParallel().ForAll(binding =>
             {
                 var numRows = binding.GetNumRowsInTable(_Adapter);
-                contents.Add(new CheckBox
+                Dispatcher.InvokeAsync(new Action(() =>
                 {
-                    Name = binding.Name + "ExportCheckBox",
-                    Content = $"{binding.Name} {(numRows > 0 ? numRows.ToString() : "")} {(numRows > 0 ? "rows " : "")}to Export\\{binding.Name}.dbc",
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Margin = new Thickness(1),
-                    IsEnabled = numRows > 0,
-                    IsChecked = numRows > 0 && IsDefaultImport(binding.Name.ToLower())
-                });
-            }
+                    CheckBox box = null;
+                    foreach (CheckBox child in contents)
+                    {
+                        if (child.Name.StartsWith(binding.Name))
+                        {
+                            box = child;
+                            break;
+                        }
+                    }
+                    if (box != null)
+                    {
+                        box.Content = $"{binding.Name} {(numRows > 0 ? numRows.ToString() : "")} {(numRows > 0 ? "rows " : "")}to Export\\{binding.Name}.dbc";
+                        box.IsEnabled = numRows > 0;
+                        box.IsChecked = numRows > 0 && IsDefaultImport(binding.Name.ToLower());
+                    }
+                }));
+            }));
         }
 
         private void MpqClick(object sender, RoutedEventArgs e)
