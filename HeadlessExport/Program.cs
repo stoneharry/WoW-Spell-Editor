b4b0de55bbf8a7a8e1762b099bacb45f13638846
stoneharry <stoneharryjames@gmail.com>
2022-12-28 21:49:41 +0000

    Improvements to HeadlessExporter logging
    - Logs percent only when different to previous value for same DBC
    - Include state (Export from SQL or Write to Disk)
    
    Sample: https://i.imgur.com/uZeKALp.png

--- HeadlessExport/Program.cs
+++ HeadlessExport/Program.cs
@@ -1,19 +1,19 @@
 ﻿using SpellEditor.Sources.Binding;
 using SpellEditor.Sources.Config;
 using SpellEditor.Sources.Database;
-using SpellEditor.Sources.DBC;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace HeadlessExport
 {
     class Program
     {
-        static Dictionary<int, string> _TaskNameLookup;
+        static ConcurrentDictionary<int, string> _TaskNameLookup;
+        static ConcurrentDictionary<int, ReportProgress> _TaskProgressLookup;
 
         static void Main(string[] args)
         {
@@ -31,13 +31,14 @@ namespace HeadlessExport
 
                 Console.WriteLine("Exporting all DBC files...");
                 var taskList = new List<Task>();
-                _TaskNameLookup = new Dictionary<int, string>();
+                _TaskNameLookup = new ConcurrentDictionary<int, string>();
+                _TaskProgressLookup = new ConcurrentDictionary<int, ReportProgress>();
                 foreach (var binding in bindingManager.GetAllBindings())
                 {
                     Console.WriteLine($"Exporting { binding.Name }...");
                     var task = new HeadlessDbc()
                         .ExportToDbc(adapter, SetProgress, binding.Fields[0].Name, binding.Name);
-                    _TaskNameLookup.Add(task.Id, binding.Name);
+                    _TaskNameLookup.TryAdd(task.Id, binding.Name);
                     taskList.Add(task);
                 }
                 Task.WaitAll(taskList.ToArray());
@@ -51,9 +52,46 @@ namespace HeadlessExport
 
         public static void SetProgress(double value)
         {
+            int reportValue = Convert.ToInt32(value * 100D);
             int id = Task.CurrentId.GetValueOrDefault(0);
             var name = _TaskNameLookup.Keys.Contains(id) ? _TaskNameLookup[id] : string.Empty;
-            Console.WriteLine($" { (name.Length > 0 ? name : $"[{ id }]") }: { Convert.ToInt32(value * 100D) }%");
+            if (_TaskProgressLookup.TryGetValue(id, out var savedProgress))
+            {
+                int state = savedProgress.State;
+                if (reportValue > savedProgress.Progress)
+                {
+                    LogProgress(name, id, reportValue, state);
+                }
+                else if (reportValue < savedProgress.Progress)
+                {
+                    ++state;
+                    LogProgress(name, id, reportValue, state);
+                }
+                _TaskProgressLookup.TryUpdate(id, new ReportProgress(reportValue, state), savedProgress);
+            }
+            else
+            {
+                _TaskProgressLookup.TryAdd(id, new ReportProgress(reportValue, 0));
+            }
+        }
+
+        public static void LogProgress(string name, int id, int reportValue, int state)
+        {
+            var stateStr = state == 0 ? "Export" : "Write";
+            var nameStr = name.Length > 0 ? name : $"[{id}]";
+            Console.WriteLine($" [{nameStr}] {stateStr}: {reportValue}%");
+        }
+
+        class ReportProgress
+        {
+            public readonly int Progress;
+            public readonly int State;
+
+            public ReportProgress(int progress, int state)
+            {
+                Progress = progress;
+                State = state;
+            }
         }
     }
 }
