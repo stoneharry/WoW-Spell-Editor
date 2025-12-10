119aa10c373d44bddb27b27b1d32d5d7ae02c984
T1ti <40864460+T1ti@users.noreply.github.com>
2025-12-10 05:54:26 +0100

    cleanup binary folder. move libs and remove xaml dll files

--- SpellGUIV2/App.xaml.cs
+++ SpellGUIV2/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿using NLog;
-using System;
+﻿using System;
+using System.Reflection;
 using System.Windows;
+using NLog;
 
 namespace SpellEditor
 {
-    public partial class App
+    public partial class App : Application
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string dllName = new AssemblyName(args.Name).Name + ".dll";
+            string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Libraries", dllName);
+            if (System.IO.File.Exists(path))
+                return Assembly.LoadFrom(path);
+            return null;
+        }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
